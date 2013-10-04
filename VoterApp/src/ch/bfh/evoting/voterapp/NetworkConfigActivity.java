c2d1497b32a6cbdae80a7e3d6fba8dec698bfbf1
Juerg Ritter <juerg_ritter@bluewin.ch>
2013-10-04 15:21:02 +0200

    introduced refresh button

--- VoterApp/src/ch/bfh/evoting/voterapp/NetworkConfigActivity.java
+++ VoterApp/src/ch/bfh/evoting/voterapp/NetworkConfigActivity.java
@@ -1,9 +1,13 @@
 package ch.bfh.evoting.voterapp;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 /**
  * Activity displaying the available networks
@@ -11,6 +15,8 @@ import android.view.MenuItem;
  *
  */
 public class NetworkConfigActivity extends Activity  {
+	
+	private WifiManager wifi;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,8 @@ public class NetworkConfigActivity extends Activity  {
 		setContentView(R.layout.activity_network_config);
 		// Show the Up button in the action bar.
 		setupActionBar();	
+		
+		wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
 	}
 
 	/**
@@ -27,6 +35,13 @@ public class NetworkConfigActivity extends Activity  {
 
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.network_config, menu);
+		return true;
 	}
 
 	@Override
@@ -42,7 +57,14 @@ public class NetworkConfigActivity extends Activity  {
 			//
 			NavUtils.navigateUpFromSameTask(this);
 			return true;
+		case R.id.rescan_wifi:
+			// rescanning the WLAN networks
+			wifi.startScan();
+			Toast.makeText(this, "Rescan initiated", Toast.LENGTH_SHORT).show();
+			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+
 }
